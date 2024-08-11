f4b2ca0f77197c5c1009136ef1478850eaea9a08
TheDarkKnight69 <s4shaayeq@gmail.com>
2024-08-11 21:03:32 +0530

    big big file commit. small progress on the response front.

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+from meta_ai_api import MetaAI
+meta = MetaAI()
+user_input = input("Input: ")
+output = meta.prompt(user_input)
+print(output['message'])
 while True:
-    print("hellooo")
+
+    user_input = input("Input: ")
+    output = meta.prompt(user_input)
+    print(output['message'])
